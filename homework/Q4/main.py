b32a0be9fcfdcb2d62bb0f8b725094d66598e89a
wolfaika <wolfblue0907@gmail.com>
2016-04-29 21:58:57 +0800

    add my sourcecode

--- homework/Q4/main.py
+++ homework/Q4/main.py
@@ -1,3 +1,146 @@
-from UnorderedListModule import UnorderedList
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
 
-print()
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+class UnorderedList(Node):
+
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+    def append(self, item):
+        current = self.head
+        previous = None
+        while current != None:
+            previous = current
+            current = current.getNext()
+
+        temp = Node(item)
+        if previous == None:
+            self.head = temp
+        else:
+            previous.setNext(temp)
+
+    def index(self, item):
+        item = Node(item)
+        current = self.head
+        count = 0
+        while current:
+            if item.getData() == current.getData():
+                return count
+            current = current.getNext()
+            count += 1
+        return None
+
+    def pop(self, pos=None):
+
+        if pos is None:
+            current_node = self.head
+            previous_node = None
+            while current_node.getNext() is not None:
+                previous_node = current_node
+                current_node = current_node.getNext()
+            data = current_node.getData()
+            previous_node.setNext(current_node.getNext())
+            return data
+        else:
+            current_node = self.head
+            previous_node = None
+            current_pos = 0
+            while current_pos != pos:
+                previous_node = current_node
+                current_node = current_node.getNext()
+                current_pos = current_pos + 1
+            data = current_node.getData()
+            previous_node.setNext(current_node.getNext())
+            return data
+
+
+    def insert(self, pos, item):
+        if pos == 0:
+            self.add(item)
+        else:
+            cont = 0
+            next = self.head
+            while pos - 1 != cont:
+                next = next.next
+                cont += 1
+            new_item = Node(item)
+            new_item.next = next.next
+            next.next = new_item
+
+mylist = UnorderedList()
+
+print(mylist.isEmpty())
+mylist.add(1722)
+mylist.add('aika')
+mylist.add(2048)
+print(mylist.size())
+mylist.append(777)
+print(mylist.search(777))
+print(mylist.size())
+mylist.remove(2048)
+print(mylist.size())
+print(mylist.index(777))
+mylist.insert(2,97)
+print(mylist.index(97))
+mylist.pop()
+print(mylist.search(777))
+mylist.pop(1)
+print(mylist.search(1722))
